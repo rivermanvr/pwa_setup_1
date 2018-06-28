651e9f4054d4e555e87efcc8c5472c08e723c8db
rivermanvr <vrgithub@riversconsulting.com>
2018-06-28 12:06:42 -0400

    setup html & short JS to register the service worker

---
+++ main.js
@@ -0,0 +1,10 @@
+//Progressive Enhancement (SW supported)
+// Check if 'service worker' is in navigator
+// if ('serviceWorker' in navigator) {}
+if (navigator.serviceWorker) {
+  // the method exists so register the serviceWorker
+  navigator.serviceWorker.register('/sw.js')
+    .then(function(registration) {
+      console.log('main.js -> SW registered');
+    });
+}
